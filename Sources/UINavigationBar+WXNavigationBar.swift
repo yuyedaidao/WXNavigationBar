de6e6bd36b8c85cca3b0ed3f348ab627b15a0d5c
WYQ <wyqpadding@gmail.com>
2022-11-07 20:57:23 +0800

    增加事件透传功能

--- Sources/UINavigationBar+WXNavigationBar.swift
+++ Sources/UINavigationBar+WXNavigationBar.swift
@@ -13,11 +13,13 @@ extension UINavigationBar {
     
     private struct AssociatedKeys {
         static var frameDidUpdated = "frameDidUpdated"
+        static var interceptTouchEvents = "interceptTouchEvents"
     }
     
     static let swizzleNavigationBarOnce: Void = {
         let cls = UINavigationBar.self
         swizzleMethod(cls, #selector(UINavigationBar.layoutSubviews), #selector(UINavigationBar.wx_layoutSubviews))
+        swizzleMethod(cls, #selector(UINavigationBar.hitTest(_:with:)), #selector(UINavigationBar.wx_hitTest(_:with:)))
     }()
     
     var frameDidUpdated: NavigationBarFrameDidUpdated? {
@@ -28,5 +30,17 @@ extension UINavigationBar {
     @objc private func wx_layoutSubviews() {
         frameDidUpdated?(frame)
         wx_layoutSubviews()
+    }
+    
+    @objc open var interceptTouchEvents: Bool {
+        get { return (objc_getAssociatedObject(self, &AssociatedKeys.interceptTouchEvents) as? Bool) ?? false }
+        set { objc_setAssociatedObject(self, &AssociatedKeys.interceptTouchEvents, newValue, .OBJC_ASSOCIATION_ASSIGN) }
+    }
+    @objc private func wx_hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if interceptTouchEvents {
+            return nil
+        } else {
+            return wx_hitTest(point, with: event)
+        }
     }
 }
